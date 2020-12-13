9c444cd1a6e96262096f87abcc3259de60e104f9
breno-do-val <brenorvale@gmail.com>
2020-12-13 16:04:53 -0300

    (feat): add activate event for service worker

--- sw.js
+++ sw.js
@@ -5,9 +5,40 @@ const assetsToCache = [
     '/'
 ];
 
+const removeOldCache = key => {
+    if(key !== cacheName) {
+        return caches.delete(key);
+    }
+};
+
+const cacheCleanUp = async () => {
+    const keyList = await caches.keys();
+    return Promise.all(keyList.map(removeOldCache));
+};
+
 self.addEventListener('install', event => {
+    console.log(`Installing service worker ${event}`);
     event.waitUntil(
         caches.open(cacheName).then(cache => cache.addAll(assetsToCache))
     );
     self.skipWaiting();
-})
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(cacheCleanUp());
+    return self.clients.claim();
+});
+
+const cacheFirst = async (request) => {
+    try {
+        const cache = await caches.open(cacheName);
+        const response = await cache.match(request);
+        return response || fetch(request);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+self.addEventListener('fetch', event => {
+    event.respondWith(cacheFirst(event.request));
+});
